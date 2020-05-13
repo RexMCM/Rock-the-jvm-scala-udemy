09113a10f4e4022aef651903a5883539e624bfae
Matheus Cardoso de Medeiros <Matheus.Cardoso@ibm.com>
2020-05-13 17:08:20 -0300

    Recursion class done

---
+++ src/lectures/part1basics/Recursion.scala
@@ -0,0 +1,39 @@
+package lectures.part1basics
+
+import scala.annotation.tailrec
+
+object Recursion extends App {
+
+  def anotherFactorial(n: Int): BigInt = {
+    def factHelper(x: Int, accumulator: BigInt): BigInt =
+      if (x<=1) accumulator
+      else factHelper(x-1, x * accumulator)
+
+    factHelper(n,1)
+  }
+
+  @tailrec
+  def concatenateTailrec(aString: String, n: Int, accumulator: String): String =
+    if (n <= 0) accumulator
+    else concatenateTailrec(aString, n-1, aString + accumulator)
+
+  println(concatenateTailrec("hello",3,""))
+
+  def isPrime(n: Int): Boolean = {
+    @tailrec
+    def isPrimeTailrec(t: Int, isStillPrime: Boolean): Boolean =
+      if (!isStillPrime) false
+      else if (t <= 1) true
+      else isPrimeTailrec(t - 1, n % t != 0 && isStillPrime)
+
+    isPrimeTailrec(n/2,true)
+  }
+
+  def fibonacci(n: Int): Int = {
+    def fiboTailrec(i: Int, last: Int, nextToLast: Int): Int =
+      if(i >= n) last
+      else fiboTailrec(i + 1, last + nextToLast, last)
+    if(n <= 2) 1
+    else fiboTailrec(2,1,1)
+  }
+}
